b9b2740aca03db7537fd6e9efc762891697e94c2
Felipe Souza <fnscoder@gmail.com>
2024-05-09 12:42:50 -0300

    Add City model on admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import City
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "state", "country", "lat", "long")
+    search_fields = ("name", "state", "country")
